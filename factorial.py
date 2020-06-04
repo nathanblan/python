739c813fd5b3380b99c2ea26076f51adc6ea7981
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-04 11:23:03 -0700

    tried to get python to work in atom (it doesnt)

---
+++ factorial.py
@@ -0,0 +1,15 @@
+#this is python stuff for finding factorial
+
+num = int(input())
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of"+ str(num) + "is" + str(factorial)
