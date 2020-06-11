34ee30a9903ed28e448a1d551c129a113f32af45
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-11 08:45:46 -0700

    Update datasci1.py

--- datasci1.py
+++ datasci1.py
@@ -1,6 +1,8 @@
 #this is some datasci stuff hehe
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 csv_path = 'https://raw.githubusercontent.com/KeithGalli/pandas/master/pokemon_data.csv'
 df = pd.read_csv(csv_path)
